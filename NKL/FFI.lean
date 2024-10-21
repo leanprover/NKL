a644ff656542d3d3ebd1a41a4126d446aede4b3c
Paul Govereau <govereau@amazon.com>
2024-10-21 07:32:16 -0400

    feat: setup basic lean-python interop
    Add a mechanism to generate python definitions using Lean
    as a reference. Using generated python definitions, add a
    mechanism to call lean from python using python FFI and
    lean RFFI.

---
+++ NKL/FFI.lean
@@ -0,0 +1,22 @@
+/-
+Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Govereau
+-/
+import Lean
+import NKL.NKI
+
+namespace NKL
+
+-- temporary for testing
+
+@[export parse_json]
+def parse_json (json : String) : IO Unit := do
+  match Lean.Json.parse json with
+  | .error str => throw $ .userError str
+  | .ok jsn => do
+    match Lean.fromJson? jsn with
+    | .error str => throw $ .userError str
+    | .ok (_:Fun) => do
+      IO.println "parse successsful"
+      return ()
