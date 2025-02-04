f04946c64a930de8f03dc7b224e72938e27262b8
Sean McLaughlin <seanmcl@amazon.com>
2025-02-03 16:16:32 -0800

    [WIP] subprocess version of python/lean communication
    Had to comment out the `assert` statements which aren't supported
    yet, but this makes the getting-started example go through
    
         $ interop/test.sh
         json file: /var/folders/hz/hgkdl8pj5fg_zlm2qh8x8fy40000gr/T/tmp6iaqtmi8.json
           4 = nki.language.load(%a_input<float64:128,512>)
           5 = nki.language.load(%b_input<float64:128,512>)
           6 = nki.language.add(4,5)
           7 = nki.language.store(%c_output<float64:128,512>,value=6)
    
    Just an idea.

--- Main.lean
+++ Main.lean
@@ -1,5 +1,33 @@
 import NKL
+import Cli
+import NKL.Python
+open Cli
 
-def main : List String -> IO Unit
-  | [ file ] => IO.FS.readFile file >>= NKL.parse_json
-  | _ => IO.println "invalid arguments"
+def parseJson (p : Parsed) : IO UInt32 := do
+  let file := p.positionalArg! "file" |>.as! String
+  let s <- IO.FS.readFile file
+  let _ <- NKL.parse_json s
+  return 0
+
+def parseJsonCmd := `[Cli|
+  "parse-json" VIA parseJson;
+  "Parse NKI kernels as JSON"
+
+  ARGS:
+    file : String;      "shape to test"
+]
+
+def klrCmd : Cmd := `[Cli|
+  klr NOOP; ["0.0.1"]
+  "KLR is an IR for NKI and other tensor-like languages in Lean."
+
+  SUBCOMMANDS:
+    parseJsonCmd
+]
+
+def main (args : List String) : IO UInt32 :=
+  if args.isEmpty then do
+    IO.println klrCmd.help
+    return 0
+  else do
+   klrCmd.validate args
--- interop/examples/getting_started.py
+++ interop/examples/getting_started.py
@@ -1,15 +1,16 @@
 import nki.language as nl
+import numpy as np
 
 def nki_tensor_add_kernel(a_input, b_input, c_output):
     """NKI kernel to compute element-wise addition of two input tensors
     """
 
     # Check all input/output tensor shapes are the same for element-wise operation
-    assert a_input.shape == b_input.shape == c_output.shape
+    # assert a_input.shape == b_input.shape == c_output.shape
 
     # Check size of the first dimension does not exceed on-chip memory tile size limit,
     # so that we don't need to tile the input to keep this example simple
-    assert a_input.shape[0] <= nl.tile_size.pmax
+    # assert a_input.shape[0] <= nl.tile_size.pmax
 
     # Load the inputs from device memory to on-chip memory
     a_tile = nl.load(a_input)
