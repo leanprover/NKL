f7a1f2f537fb8a68ae27ce29454baa6a2fb3c024
Sean McLaughlin <seanmcl@amazon.com>
2025-02-05 13:48:08 -0800

    Add CLI library
    This allows us to add more subcommands for things like
    linting and lsp serving. No new commands added yet.

--- Main.lean
+++ Main.lean
@@ -1,5 +1,44 @@
 import KLR
+import Cli
+import KLR.Python
+open Cli
 
-def main : List String -> IO Unit
-  | [ file ] => IO.FS.readFile file >>= KLR.parse_json
-  | _ => IO.println "invalid arguments"
+local instance : MonadLift Err IO where
+  monadLift
+    | .ok x => return x
+    | .error s => throw $ .userError s
+
+private def parseJsonString (s : String) : IO Unit := do
+  let kernel <- KLR.Python.Parsing.parse s
+  let stmts <- KLR.Trace.runNKIKernel kernel
+  for s in stmts do
+    IO.println ("  " ++ repr s)
+
+def parseJsonFile (p : Parsed) : IO UInt32 := do
+  let file := p.positionalArg! "file" |>.as! String
+  let s <- IO.FS.readFile file
+  let _ <- parseJsonString s
+  return 0
+
+def parseJsonCmd := `[Cli|
+  "parse-json" VIA parseJsonFile;
+  "Parse KLR kernels as JSON"
+
+  ARGS:
+    file : String;      "File of Python AST printed as JSON"
+]
+
+def klrCmd : Cmd := `[Cli|
+  klr NOOP; ["0.0.1"]
+  "KLR is an IR for NKI and other tensor-like languages in Lean."
+
+  SUBCOMMANDS:
+    parseJsonCmd
+]
+
+def main (args : List String) : IO UInt32 :=
+  if args.isEmpty then do
+    IO.println klrCmd.help
+    return 0
+  else do
+   klrCmd.validate args
--- lakefile.lean
+++ lakefile.lean
@@ -6,8 +6,9 @@ package "KLR" where
 lean_lib "KLR" where
   defaultFacets := #[LeanLib.staticFacet]
 
---lean_lib "Export" where
-
 @[default_target]
 lean_exe "klr" where
   root := `Main
+
+require Cli from git
+  "https://github.com/leanprover/lean4-cli.git" @ "v2.2.0-lv4.14.0-rc1"
